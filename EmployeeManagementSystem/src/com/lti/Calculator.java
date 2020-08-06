02d2e8c413368c5af8e305273f7c00f6d99824ed
apple <apple@localhost>
2020-08-06 09:47:11 +0530

    added diff method

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
 		System.out.println("Sum: " + (a + b));
 	}
 	
+	public static void diff(int a , int b) {
+		System.out.println("Sum: " + (a - b));
+	}
+	
 	public static void main(String[] args) {
 		add(10,30);
 	}
